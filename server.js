1fead50778e7e29d2aed7f3b39fdfb87e785f93f
Mayank <mayank@example.com>
2026-04-09 11:34:11 +0530

    Add SSL parameter required by Aiven

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const db = mysql.createConnection({
     port: process.env.DB_PORT || 3307,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'ocr_db'
+    database: process.env.DB_NAME || 'ocr_db',
+    ssl: process.env.DB_HOST ? { rejectUnauthorized: true } : undefined
 });
 
 db.connect(err => {
